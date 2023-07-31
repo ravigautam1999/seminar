d5cb45ab98ca0c68ebc33900fc64fed763f719db
ravigautam1999 <ravigautam.01.1999@gmail.com>
2023-07-31 12:14:43 +0530

    first changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,76 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MdbAccordionModule } from 'mdb-angular-ui-kit/accordion';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbModalModule } from 'mdb-angular-ui-kit/modal';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbRadioModule } from 'mdb-angular-ui-kit/radio';
+import { MdbRangeModule } from 'mdb-angular-ui-kit/range';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
+import { MdbScrollspyModule } from 'mdb-angular-ui-kit/scrollspy';
+import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
+import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { SingleBlogComponent } from './single-blog/single-blog.component';
+import { BlogCardComponent } from './blog-card/blog-card.component';
+import { SingleSpeakerComponent } from './single-speaker/single-speaker.component';
+import { SingleScheduleComponent } from './single-schedule/single-schedule.component';
+import { SingleTicketComponent } from './single-ticket/single-ticket.component';
+import { HomeComponent } from './home/home.component';
+import { FormFieldComponent } from './form-field/form-field.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormFieldComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    NavigationComponent,
+    ScheduleComponent,
+    SpeakersComponent,
+    TicketsComponent,
+    SingleBlogComponent,
+    BlogCardComponent,
+    SingleSpeakerComponent,
+    SingleScheduleComponent,
+    SingleTicketComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbAccordionModule,
+    MdbCarouselModule,
+    MdbCheckboxModule,
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbFormsModule,
+    MdbModalModule,
+    MdbPopoverModule,
+    MdbRadioModule,
+    MdbRangeModule,
+    MdbRippleModule,
+    MdbScrollspyModule,
+    MdbTabsModule,
+    MdbTooltipModule,
+    MdbValidationModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
